301a5c27bafab6575850470e7f84b50c7df95f43
PranavkrishnaVadhyar <bpranavkrishnavadhyar@gmail.com>
2025-08-06 23:10:19 +0530

    CRUD 1

--- FastAPI_Advanced/main.py
+++ FastAPI_Advanced/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from pydantic import BaseModel
+from typing import List
+import json
 
 app = FastAPI()
 
@@ -9,23 +11,43 @@ async def read_root():
 
     return {"message" : "Hello world"}
 
-@app.get('/greet/{name}') # Dynamic URL
-async def greet_name(name:str, age:int) -> dict:
-
-    return {'messsge' : f"Hello {name} {age}"}
 
 class BookCreateModel(BaseModel):
     title : str
     author : str
+ 
+class Book(BaseModel):
+    id : int
+    title : str
+    author : str
+    page_count : int
 
-
-@app.post('/create_book')
-async def create_book(book_data:BookCreateModel):
+@app.get('/books', response_model=List[Book])
+async def get_all_books():
     
-    return {
-        "title" : book_data.title,
-        "author" : book_data.author
-    } 
+    try:
+        with open('data.json', "r") as f:
+            data = json.load(f)
+        
+        return {'data' : data}
+    
+    except FileNotFoundError:
+        return {'error' : 'File not found!'} 
+    
 
-@app.get('/get')
-async def 
+
+@app.get('/books/{bookid}')
+async def get_books(book_id:int) -> dict:
+    pass
+
+@app.post('/books', status_code=status.HTTP_201_CREATED)
+async def create_book(book_data:Book):
+    
+    with open('data.json', 'w') as f:
+        
+        data = json.dump(f, book_data.model_dump_json)
+
+    if not data:
+        return {'error' : 'Not added'}
+    
+    return {'message' : 'suceess', 'data' : book_data.model_dump_json}
