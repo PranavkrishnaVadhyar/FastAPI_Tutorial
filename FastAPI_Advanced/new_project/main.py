ddea9589a986e3781980f5fb308739c5283f558a
PranavkrishnaVadhyar <bpranavkrishnavadhyar@gmail.com>
2025-08-14 00:18:42 +0530

    new project

---
+++ FastAPI_Advanced/new_project/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+def index():
+    return {"data" : 'blog_list'}
+
+
+@app.get('/blog/{id}')
+def show(id : int):
+    return {'data' : id}
+
+
