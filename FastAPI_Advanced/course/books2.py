86dcf6540d1dde2f7cf1b0c1d101b375267fc05a
PranavkrishnaVadhyar <bpranavkrishnavadhyar@gmail.com>
2025-08-20 00:48:13 +0530

    Pydantic 1

--- FastAPI_Advanced/course/books2.py
+++ FastAPI_Advanced/course/books2.py
@@ -1,2 +1,45 @@
 from  fastapi import FastAPI
-from pydantic import 
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+    def __init__(self, id, title, author, description, rating):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+
+BOOKS = [
+    Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5),
+    Book(2, 'Be Fast with FastAPI', 'codingwithroby', 'A great book!', 5),
+    Book(3, 'Master Endpoints', 'codingwithroby', 'A awesome book!', 5),
+    Book(4, 'HP1', 'Author 1', 'Book Description', 2),
+    Book(5, 'HP2', 'Author 2', 'Book Description', 3),
+    Book(6, 'HP3', 'Author 3', 'Book Description', 1)
+]
+
+class CreateBook(BaseModel):
+    id: int 
+    title: str
+    author: str
+    description: str
+    rating: int
+
+
+@app.get('/books')
+async def get_books():
+    return BOOKS
+
+@app.post('/create_book')
+async def get_books(book_request: CreateBook):
+    new_book = Book(**book_request.dict())
+    BOOKS.append(new_book)
+
