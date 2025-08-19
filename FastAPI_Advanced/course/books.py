a989ce1cf66d78237da7139f04570742c32cd023
PranavkrishnaVadhyar <bpranavkrishnavadhyar@gmail.com>
2025-08-20 00:01:59 +0530

    Books 1 project completed

--- FastAPI_Advanced/course/books.py
+++ FastAPI_Advanced/course/books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 
 app = FastAPI()
@@ -23,7 +23,48 @@ async def first_api():
 async def read_all_books():
     return BOOKS
 
-@app.get('/books/{book_id}')
-async def read_all_books(book_id):
-    return BOOKS[book_id]
+@app.get('/books/{book_title}')
+async def read_all_books(book_title : str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
 
+# Get all books from a specific author using path or query parameters
+@app.get("/books/byauthor/")
+async def read_books_by_author_path(author: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == author.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
+
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and \
+                book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
+
+@app.post("/book/create_book")
+async def add_book(book = Body()):
+    BOOKS.append(book)
+
+
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
+
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
---
+++ FastAPI_Advanced/notes.md
@@ -0,0 +1,8 @@
+# Query parameters
+
+- Request parameters with a ?
+- localhost:8000/books/?category=science
+- You can combine both query and path parameters eg: localhost:8000/books/Author%20One/?category=science
+
+
+# POST
