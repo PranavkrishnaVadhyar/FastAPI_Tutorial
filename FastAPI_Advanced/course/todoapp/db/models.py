e3ab713cf328246ccad52e36f0d830a72974f8a8
PranavkrishnaVadhyar <bpranavkrishnavadhyar@gmail.com>
2025-11-05 21:09:57 +0530

    user db

--- FastAPI_Advanced/course/todoapp/db/models.py
+++ FastAPI_Advanced/course/todoapp/db/models.py
@@ -1,6 +1,18 @@
 from db.database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeginKey
 
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
 
 class Todos(Base):
     __tablename__ = 'todos'
@@ -9,7 +21,9 @@ class Todos(Base):
     title = Column(String)
     description = Column(String)
     priority = Column(Integer)
-    complete = Column(Boolean)
+    complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeginKey("users.id"))
+
 
 
 
